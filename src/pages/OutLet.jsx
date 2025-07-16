9c83019679b91392eac00c709dc6959c50a3dd10
OwaisAbdullah644 <anonymoushackerunknown10@gmail.com>
2025-07-16 23:28:51 +0500

    Updated outlet

--- src/pages/OutLet.jsx
+++ src/pages/OutLet.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import Navbar from './Navbar'
 
 const OutLet = () => {
   return (
     <>
-      <Footer/>
-      <OutLet/>
-      <Navbar/>
+      <Navbar />
+      <Outlet /> 
+      <Footer />
     </>
   )
 }
